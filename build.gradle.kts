b9b7b6c1481f3a7b8e1259dd8e0d7dd0bccd0a90
Juuz <6596629+Juuxel@users.noreply.github.com>
2022-04-30 13:18:54 +0300

    Update Gradle and Kotlin, remove Bintray

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,12 @@
-import com.jfrog.bintray.gradle.BintrayExtension
 import org.jetbrains.dokka.gradle.DokkaTask
 
 plugins {
     `java-library`
     `maven-publish`
 
-    kotlin("jvm") version "1.4.21"
-    id("org.jetbrains.dokka") version "1.4.20"
-    id("org.jmailen.kotlinter") version "3.3.0"
-    id("com.jfrog.bintray") version "1.8.5"
+    kotlin("jvm") version "1.6.21"
+    id("org.jetbrains.dokka") version "1.6.21"
+    id("org.jmailen.kotlinter") version "3.10.0"
 }
 
 group = "io.github.juuxel"
@@ -19,14 +17,14 @@ java {
 }
 
 repositories {
-    jcenter()
+    mavenCentral()
 }
 
 dependencies {
     api(kotlin("stdlib"))
 
-    val spek = "2.0.15"
-    testImplementation("io.strikt:strikt-core:0.28.1")
+    val spek = "2.0.18"
+    testImplementation("io.strikt:strikt-core:0.34.0")
     testImplementation("org.spekframework.spek2:spek-dsl-jvm:$spek")
     testRuntimeOnly("org.spekframework.spek2:spek-runner-junit5:$spek")
     testRuntimeOnly(kotlin("reflect"))
@@ -58,26 +56,4 @@ publishing {
     publications.create<MavenPublication>("maven") {
         from(components["java"])
     }
-}
-
-bintray {
-    if (project.hasProperty("bintrayUser")) {
-        user = project.property("bintrayUser").toString()
-        key = project.property("bintrayKey").toString()
-    } else {
-        println("'bintrayUser' not found -- please set up 'bintrayUser' and 'bintrayKey' before publishing")
-    }
-
-    pkg(closureOf<BintrayExtension.PackageConfig> {
-        repo = "maven"
-        name = "leaf-through"
-        setLicenses("MIT")
-        vcsUrl = "https://github.com/Juuxel/LeafThrough"
-
-        version(closureOf<BintrayExtension.VersionConfig> {
-            name = project.version.toString()
-        })
-    })
-
-    setPublications("maven")
 }
